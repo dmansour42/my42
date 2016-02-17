5e7490a3264dfec3952380e5e2f8bebcc645e188
Driss MANSOURI <dmansour@e2r2p22.42.fr>
2016-02-17 18:42:48 +0100

    auto ln

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+ln -svf ~/my42/.bashrc ~/.bashrc
+ln -svf ~/my42/.vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +all
